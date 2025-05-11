873afd1f1f6f9e19e6e33347aeda1fde895bc77c
Lennard van Hal <levanhal@tudelft.nl>
2025-05-12 00:51:28 +0200

    fix:Remove version endpoint

--- app.py
+++ app.py
@@ -38,21 +38,4 @@ def get_prediction():
 		return str(e), 500
 
 
-@app.route("/version", methods=["GET"])
-def get_lib_version():
-	"""
-	Gets the version from the lib-version package.
-	---
-	responses:
-		200:
-			description: Version (v#.#.#) from lib-version.
-		500:
-			description: Version could not be retrieved.
-	"""
-	try:
-		return VersionUtil.get_version()
-	except Exception as e:
-		print(e)
-		return "Version not found", 500
-
 app.run(host="0.0.0.0", port=5000)
