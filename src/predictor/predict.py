a5bf772c573189d862828f840cb8348f2fb6bb4a
LvHTUD <33719406+LvHTUD@users.noreply.github.com>
2025-05-24 20:01:04 +0100

    Account for application of Cookiecutter template in model-training (#9)
    * Change URL to trained models
    
    * fix:Update model URL to new URLs in model-training
    
    ---------
    
    Co-authored-by: Lennard van Hal <levanhal@tudelft.nl>

--- src/predictor/predict.py
+++ src/predictor/predict.py
@@ -6,11 +6,17 @@ from lib_ml.preprocessing import preprocess_text
 class ReviewSentimentPredictor:
     def __init__(self):
         # Load trained model to generate predictions with
-        model_file = urlopen("https://github.com/remla25-team3/model-training/raw/refs/heads/main/model_training/data/sentiment_model.pkl")
+        model_file = urlopen(
+            'https://raw.githubusercontent.com/remla25-team3/model-training/refs/heads/main' +
+            '/models/sentiment_model.pkl'
+        )
         self.prediction_model = joblib.load(model_file)
 
         # Sentiments model to transform string inputs into numbers
-        sentiments_file = urlopen("https://github.com/remla25-team3/model-training/raw/refs/heads/main/model_training/data/c1_BoW_Sentiment_Model.pkl")
+        sentiments_file = urlopen(
+            'https://raw.githubusercontent.com/remla25-team3/model-training/refs/heads/main' +
+            '/models/bow_sentiment_model.pkl'
+        )
         self.sentiment_model = joblib.load(sentiments_file)
 
     def predict(self, user_input: str):
@@ -28,10 +34,10 @@ class ReviewSentimentPredictor:
         if user_input == '':
             raise ValueError('Input should not be empty')
 
-        preprocessed = [preprocess_text(user_input)]
+        preprocessed = [preprocess_text(user_input)] # Apply lib-ml preprocessing
         transformed = self.sentiment_model.transform(preprocessed).toarray()
-        X = np.array(transformed, dtype=object).reshape(1, -1) # Apply lib-ml preprocessing
-        predictions = self.prediction_model.predict(X)
+        x = np.array(transformed, dtype=object).reshape(1, -1)
+        predictions = self.prediction_model.predict(x)
 
         if len(predictions) == 0:
             raise Exception('Error generating prediction')
